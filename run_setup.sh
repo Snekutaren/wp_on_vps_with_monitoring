1b38f605e8daa2e2ed5ab0641b13139465f6fe61
Snekutaren <snekutaren@gmail.com>
2025-07-18 12:00:55 +0000

    run_setup.sh

--- run_setup.sh
+++ run_setup.sh
@@ -307,7 +307,7 @@ main() {
 
     local CUSTOM_APP_BASE_NAME_ARG=""
     local APP_NAME_SUFFIX_ARG=""
-    local PORT_OFFSET_ARG=0
+    PORT_OFFSET_ARG=0
 
     # Parse command-line options
     while getopts "b:a:n:o:" opt; do
